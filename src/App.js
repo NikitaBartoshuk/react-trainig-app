6d9b376383a8a8930d37777407a668aaf28d3c37
kakulya14 <nbartoshuk@mail.ru>
2022-06-25 16:06:53 +0300

    project init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useMemo, useRef, useState} from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import  './styles/App.css';
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
+import {useFetching} from "./hooks/useFetching";
 
 function App() {
+
+    const [posts, setPosts] = useState([])
+
+    const [filter, setFilter] = useState({sort: '', query: ''})
+    const [modal, setModal] = useState(false);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+    const [totalCount, setTotalCount] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [page, setPage] = useState(1);
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const response = await PostService.getAll(limit, page);
+        setPosts(response.data)
+        console.log(response.headers['x-total-count'])
+        setTotalCount(response.headers['x-total-count'])
+    })
+
+    useEffect(() => {
+        fetchPosts()
+    }, [])
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+        setModal(false);
+    }
+
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+            Создать пользователя
+        </MyButton>
+        <button></button>
+        <MyModal visible={modal} setVisible={setModal}>
+            <PostForm create={createPost}/>
+        </MyModal>
+        <hr style={{margin: '15px 15px'}}/>
+        <PostFilter
+            filter={filter}
+            setFilter={setFilter}
+        />
+        {postError &&
+            <h1>Произошла ошибка ${postError}</h1>
+        }
+        {isPostsLoading
+            ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
+            : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
+        }
     </div>
   );
 }
